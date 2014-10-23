47354a645d05e8b23591c5efd3d4c04e04317516
fintalex <fintalex@mail.ru>
2014-10-23 17:31:24 +0400

    Комменты по коду
    Комменты к методам

--- src/computer.js
+++ src/computer.js
@@ -1,38 +1,40 @@
+// создаем глобальную переменную КОмпутер
+
 window.Computer = {
-	currentMemory: 2,
-	currentSpace: 1000,
-	addMemory: function() {
-		var memory = this.currentMemory;
+	currentMemory: 2, // свойство - количество памяти в GB
+	currentSpace: 1000, // свойство - количество памяти
+	addMemory: function() { // метод - добавление слотов памяти, один параметр - один слот
+		var memory = this.currentMemory; // кэшируем нашу память
 		
-		if (arguments.length > 2){
+		if (arguments.length > 2){  // проверка если слотов больше двух передали - то нафиг, у нас только два слота
 			return false;
 		}
 
 		for (var i = 0, len = arguments.length; i < len ; i++) {
-			memory += arguments[i];
+			memory += arguments[i]; // если все ок - увеличиваем нашу память
 		};
 
-		this.currentMemory = memory;
-		return this.currentMemory;
+		this.currentMemory = memory; // присваиваем нашей памяти компа - память с которой работали
+		return this.currentMemory; // возвращаем текущую память
 	}, 
-	instal: function () {
+	instal: function () { // метод установки ПО, также может принимать несколько объектов ПО
 		for (var i= 0, len = arguments.length; i < len; i++) {
-			if (arguments[i].space <= this.currentSpace){
+			if (arguments[i].space <= this.currentSpace){ // если у нас есть свободное пространство ,то мы можем установить ПО
 
-				this.currentSpace -= arguments[i].space;
-				arguments[i].installed = true;
+				this.currentSpace -= arguments[i].space; // уменьшаем свободное пространство на диске компа
+				arguments[i].installed = true; // устанавливаем текущему объекту свойсво - установлен
 			}
 		}
 
 		for (var i= 0, len = arguments.length; i < len; i++) {
-			if (arguments[i].installed == false) {
-				return false;
+			if (arguments[i].installed == false) { // проверяем все ли по установлены
+				return false; // если хоть одно ПО не установилось - возвращаем false
 			}
 		}
 
-		return true;
+		return true; // если все четко установилось - возвращаем true
 	},
-	formatDrive: function(){
+	formatDrive: function(){ // мутод очистки памяти компа - форматирование диска
 		this.currentSpace = 1000;
 	}
 }
